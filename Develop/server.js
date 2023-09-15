2a4c1d0dc6a19a98aac9f7877a62b89cc594d99b
Nicolas Alonzo <nicalonzo80@yahoo.com>
2023-09-14 19:48:14 -0500

    Finalized Project

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const Sequelize = require('sequelize')
+const sequelize = require('./config/connection')
 // import sequelize connection
 
 const app = express();
@@ -10,8 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force:false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
